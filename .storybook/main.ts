13977ed9b0d70d1fb791e14faa81a39ebafb0b14
Badal Kumar Behera <badalkumar.behera@karboncard.com>
2025-10-01 14:42:05 +0530

    add storybook for button and reinstall the nextjs and react to its stable version

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+import type { StorybookConfig } from "@storybook/nextjs-vite";
+
+const config: StorybookConfig = {
+  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    // "@storybook/addon-vitest",
+     'storybook-addon-next',
+    "@storybook/addon-styling-webpack",
+  ],
+  framework: {
+    name: "@storybook/nextjs-vite",
+    options: {},
+  },
+};
+export default config;
